877dac5bdba339390b6e7656bcd1881624860fc0
uzuegbu joseph <josephuzuegbu55@gmail.com>
2021-10-06 15:25:47 +0100

    deleted:    public/favicon.ico         modified:   public/index.html         deleted:    public/logo192.png         deleted:    public/logo512.png         deleted:    public/manifest.json         deleted:    public/robots.txt         modified:   src/App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-
+import React from 'react';
+import Header from './component/Header';
 
 function App() {
   return (
-    <div className="App">
-     <h2>Hello World</h2>
+    <div className="container">
+     <Header/>
     </div>
   );
 }
---
+++ src/component/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './header.css'
+
+export default function Header() {
+   
+    return (
+        <div className='text-center' id='header-wrapper'>
+            <h3 className='fs-1'>Joe Trello</h3>
+            <h5 className='fs-3'>Manage your software/App life cycle </h5>
+        </div>
+    )
+}
