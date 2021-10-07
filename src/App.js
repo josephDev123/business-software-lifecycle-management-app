4dac8e2f461f0a25c894832a72e40664a39b1309
uzuegbu joseph <josephuzuegbu55@gmail.com>
2021-10-07 05:50:41 +0100

    modified:   public/index.html         modified:   src/App.js         modified:   src/component/Header.js         modified:   src/component/app.css

--- src/App.js
+++ src/App.js
@@ -1,27 +1,46 @@
 import React from 'react';
 import './component/app.css'
 import Header from './component/Header';
+import { useState, useRef } from 'react';
 
 function App() {
-  return (
+  const form_ref =useRef()
+  const [showAddContentForm, setshowAddContentForm] =useState(false);
+  //hide form
+const showForm =()=>{
+  setshowAddContentForm(true)
+}
+
+const hideForm =()=>{
+  setshowAddContentForm(false)
+}
+
+   return (
     <div className="container">
      <Header/>
      <div className='row'>
         <div className='col-sm-3 app_bg_color' id='backlog'>
           <h5>Backlog</h5>
-          <form>
-                <div className="mb-3">
-                  <label for="title" className="form-label">Title</label>
-                  <input type="text" className="form-control" id="title" aria-describedby="title"/>
-                 
-                </div>
-                <div className="mb-3">
-                  <label for="content" className="form-label">Content</label>
-                  <textarea className="form-control" id="content" />
-                </div>
-                
-                <button type="submit" className="btn btn-primary">Submit</button>
-            </form>
+          <div className="d-grid gap-2">
+            <button className='btn btn-secondary btn-sm' onClick={showForm}>+</button>
+          </div>
+          {showAddContentForm===true?
+              <form ref={form_ref}>
+                  <div className="mb-3">
+                    <input type="text" className="form-control" id="title" aria-describedby="title" placeholder='Title'/>
+                  
+                  </div>
+                  <div className="mb-3">
+                    <textarea className="form-control" id="content" placeholder='Content'/>
+                  </div>
+                  <div className="btn-group">
+                    <button className="btn btn-primary btn-sm" style={{ marginRight:'10px' }}>Add</button>
+                    <button className="btn btn-danger btn-sm" onClick={hideForm}>Cancel</button>
+                  </div>
+            </form>:''
+        
+        }
+          
         </div>
        
         <div className='col-sm-3 app_bg_color' id='progress'>
--- src/component/Header.js
+++ src/component/Header.js
@@ -5,7 +5,7 @@ export default function Header() {
    
     return (
         <div className='text-center' id='header-wrapper'>
-            <h3 className='fs-1'>Joe Trello</h3>
+            <h3 className='fs-1'>Joe Trello ❤️</h3>
             <h5 className='fs-3'>Manage your software/App life cycle </h5>
         </div>
     )
