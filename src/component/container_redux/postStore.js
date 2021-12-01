4911ce456182ff2e8d0ee8f860c036e323626d72
uzuegbu joseph <josephuzuegbu55@gmail.com>
2021-12-01 14:20:07 +0100

    modified:   package-lock.json         modified:   package.json         modified:   src/component/container_redux/postStore.js

--- src/component/container_redux/postStore.js
+++ src/component/container_redux/postStore.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { configureStore } from '@reduxjs/toolkit';
 import postReducer from './PostSlice';
 import {saveState} from './LocalStorage';
+import { throttle } from 'throttle-debounce';
 
 export const PostStore= configureStore({
     reducer:{
@@ -9,6 +10,6 @@ export const PostStore= configureStore({
     }
 })
 
-PostStore.subscribe(()=>{
+PostStore.subscribe(throttle(()=>{
     saveState(PostStore.getState().post);
-})
+}))
