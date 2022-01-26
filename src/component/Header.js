bb386d46943bf6a4609e3f30995859f05367b811
uzuegbu joseph <josephuzuegbu55@gmail.com>
2022-01-26 14:17:44 +0100

    modified:   src/component/Header.js

--- src/component/Header.js
+++ src/component/Header.js
@@ -6,7 +6,7 @@ export default function Header() {
     return (
         <div className='text-center' id='header-wrapper'>
             <h3 className='fs-1'>Joe Trello ❤️</h3>
-            <h6 className='fs-4'>Manage your software/App life cycle(Drag and Drop method) </h6>
+            <h6 className='fs-6'>Manage your software/App life cycle(Drag and Drop method) </h6>
             <div id="" className="form-text text-danger">You can use this app/softaware to manage your business and daily-to-day activities.</div>
         </div>
     )
